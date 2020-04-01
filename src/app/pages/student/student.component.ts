f73ed06597e0d653b8395f72ef2705021745d5df
syarafina <syarafina1800016124@webmail.uad.ac.id>
2020-04-01 21:27:09 +0700

    commit2

---
+++ src/app/pages/student/student.component.ts
@@ -0,0 +1,15 @@
+ import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
